ac00c548f7cf91c64e5a5cd3e1651b0f37376e95
Joe <joe@brainlabsdigital.com>
2020-11-12 15:11:34 +0000

    pull prefixes into helper file

---
+++ AmazonAdvertisingApi/RequestPrefixes.php
@@ -0,0 +1,12 @@
+<?php
+namespace AmazonAdvertisingApi;
+
+class RequestPrefixes
+{
+    public $requestPrefixStrings = array(
+    	"display" => "sd/"
+        "products" => "v2/sp/"
+        "brands"   => "sb/"
+        "profile" => "v2/"
+    );
+}
